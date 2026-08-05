d522b41749b40d4890aa8de67b51bd44d02e9ca9
Conti2024 <contiprogra@gmail.com>
2026-08-05 12:30:45 -0600

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Navbar    from './components/Navbar'
 import Hero      from './components/Hero'
 import Experience from './components/Experience'
-import Projects  from './components/Projects'
 import Skills    from './components/Skills'
+import Projects  from './components/Projects'
 import Contact   from './components/Contact'
 
 export default function App() {
